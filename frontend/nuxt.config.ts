ca1a1cbaa09890d25a0f0a8bfed85982d0e0c974
Belchenkov <u608110@gmail.com>
2024-09-17 12:35:13 +0400

    Add Env Variables

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,4 +6,10 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
   ],
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL,
+      BASE_URL: process.env.BASE_URL
+    }
+  },
 })
